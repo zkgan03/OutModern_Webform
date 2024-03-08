47a7c07e1b6642c9a94de3146fcac56b5cecf2db
zhiken <zhiken03@hotmail.com>
2024-03-09 03:20:47 +0800

    added header design, inlcuded fonts-family, font awesome

--- OutModern/src/Client/Client.Master.cs
+++ OutModern/src/Client/Client.Master.cs
@@ -11,6 +11,31 @@ namespace OutModern.Client
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                string currentUrl = Request.Url.PathAndQuery;
+                string homeUrl = ResolveUrl(hyperlinkHome.NavigateUrl);
+                string aboutUrl = ResolveUrl(hyperlinkAbout.NavigateUrl);
+
+                if (currentUrl == homeUrl)
+                {
+                    hyperlinkHome.CssClass = hyperlinkHome.CssClass.Replace("top-nav-item", "top-nav-item-active");
+                }
+                else if (currentUrl == aboutUrl)
+                {
+                    hyperlinkAbout.CssClass = hyperlinkAbout.CssClass.Replace("top-nav-item", "top-nav-item-active");
+                }
+            }
+
+        }
+
+        protected void lBtnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTest_Click(object sender, EventArgs e)
+        {
 
         }
     }
