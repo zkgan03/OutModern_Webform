108a9a4cf32faa070a143dedef1464d74999f33b
zhiken <zhiken03@hotmail.com>
2024-05-05 00:22:32 +0800

    resolve err

--- OutModern/src/Client/ClientMaster/Client.Master.cs
+++ OutModern/src/Client/ClientMaster/Client.Master.cs
@@ -12,29 +12,11 @@ namespace OutModern.src.Client.ClientMaster
     public partial class Client : System.Web.UI.MasterPage
     {
         protected string homeUrl = "~/src/Client/Home/Home.aspx";
-        protected string aboutUrl = "#";
-        protected string feedbackUrl = "#";
-        protected string cartUrl = "#";
-        int customerId = 1;
-        string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-
         string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
         private int customerId;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            //For visibility of log out base on customer logged in or not
-            //// Check for CustID cookie on every page load
-            //HttpCookie custIDCookie = Request.Cookies["CustID"];
-            //// Set HyperLink9 visibility based on cookie existence
-            //HyperLink9.Visible = custIDCookie != null;
-            //HyperLink5.Visible = custIDCookie != null;
-            //HyperLink6.Visible = custIDCookie != null;
-
-
-            //HyperLink7.Visible = custIDCookie == null;
-            //HyperLink8.Visible = custIDCookie == null;
-
             if (!IsPostBack)
             {
                 string currentUrl = Request.Url.PathAndQuery;
@@ -43,12 +25,6 @@ namespace OutModern.src.Client.ClientMaster
                 {
                     hyperlinkHome.CssClass = hyperlinkHome.CssClass.Replace("top-nav-item", "top-nav-item-active");
                 }
-                else if (currentUrl == aboutUrl)
-                {
-                    hyperlinkAbout.CssClass = hyperlinkAbout.CssClass.Replace("top-nav-item", "top-nav-item-active");
-                }
-
-
             }
 
             if (Session["CUSTID"] != null)
@@ -88,7 +64,7 @@ namespace OutModern.src.Client.ClientMaster
         protected void lBtnSearch_Click(object sender, EventArgs e)
         {
             string searchQuery = txtSearch.Text.Trim();
-            if (DetectSQLInjection(searchQuery) || DetectXSS(searchQuery) )
+            if (DetectSQLInjection(searchQuery) || DetectXSS(searchQuery))
             {
                 LockUserAccount(); // if detected update user status to "Locked" and clear the login session
                 return;
@@ -117,7 +93,7 @@ namespace OutModern.src.Client.ClientMaster
                     {
                         HttpContext.Current.Session.Clear(); // Remove session and redirect to home page
                         HttpContext.Current.Session.Abandon();
-                        HttpContext.Current.Response.Redirect("~/src/Client/Home/Home.aspx", true);                 
+                        HttpContext.Current.Response.Redirect("~/src/Client/Home/Home.aspx", true);
                     }
                 }
             }
