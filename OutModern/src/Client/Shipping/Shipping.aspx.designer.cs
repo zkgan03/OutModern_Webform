247eb10ea8a62daa116655d14c92835960d03e3b
b1ank3r <75467178+b1ank3r@users.noreply.github.com>
2024-05-06 20:17:08 +0800

    Added snackbar, and label for applied promocode

--- OutModern/src/Client/Shipping/Shipping.aspx.designer.cs
+++ OutModern/src/Client/Shipping/Shipping.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace OutModern.src.Client.Shipping
         protected global::System.Web.UI.ScriptManager ScriptManager1;
 
         /// <summary>
+        /// lblSnackbar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSnackbar;
+
+        /// <summary>
         /// SiteMapPath1 control.
         /// </summary>
         /// <remarks>
